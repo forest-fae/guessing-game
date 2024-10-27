896a02e37b0ca1877afce8dc0c64b1007be314b4
forest-fae <surreal.autumn@gmail.com>
2024-10-27 22:10:07 +0500

    init: start guessing-game task, add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,34 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.middleNum = 0;
     }
 
+    setRange(min, max) {
+        this.min = min; 
+        this.max = max; 
+     
+    }
+ 
     guess() {
-
+        this.middleNum = Math.round((this.min + this.max) / 2);
+        return this.middleNum;
+        
     }
 
     lower() {
-
+        this.max = this.middleNum; 
     }
 
     greater() {
-
+        this.min = this.middleNum;
+        
     }
 }
 
 module.exports = GuessingGame;
+
+ //ищу среднее число из диапазона, в тесте запускается guess(). 
+        //если загаданное число > middleNum, тест запускает greater(), если < - то lower(). 
+        //и так, пока middleNum не будет равен загаданному числу.
